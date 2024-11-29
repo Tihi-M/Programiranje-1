23e1e1421a16da8d78869bca367ff20e77059c95
Tihi-M <148490644+Tihi-M@users.noreply.github.com>
2024-11-29 16:20:33 +0100

    stampaci

--- binTrazenje4/main.cpp
+++ binTrazenje4/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 
-using namespace std;
+bool printed(int n,int x,int y,int z){
+    int prvi = z/x;
+    int drugi = z/y;
+    int oba = z/(x+y);
+    int ukupno = prvi+drugi-oba;
+    return ukupno >= n;
+
+}
+void copyBin(int brKopija, int prvi,int drugi){
+    int left = 1;
+    int right = brKopija;
+    int rez=1;
+    int pos;
+    while(left<=right){
+         pos =(right+left)/2;
+        if(printed(brKopija,prvi,drugi,pos)){
+            rez=pos;
+            right = pos-1;
+        }
+        else left = pos+1;
+    }
+    std::cout<<rez;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int brKopija,prvi,drugi;
+    std::cout<<"Broj kopija, brzina prvog, brzina drugog:";
+    std::cin>>brKopija>>prvi>>drugi;
+
+    copyBin(brKopija,prvi,drugi);
     return 0;
 }
